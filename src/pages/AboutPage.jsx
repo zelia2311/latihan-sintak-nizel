66fa0f310d6b648b2a9b4118dcf1503d651dd7f3
Nizelia <zeliamimpolsri@gmail.com>
2025-05-27 15:39:51 +0700

    satu

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,7 @@ import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import RafiNameCard from "../components/name-card/rafi";
 import Namecard from "../components/name-card/nizel";
-=======
+
 
 import MuteaNamecard from "../components/name-card/mutea";
 
