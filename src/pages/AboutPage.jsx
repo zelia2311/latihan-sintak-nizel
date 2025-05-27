085cda765ba73c13a7b247376715b49ae36a6c7f
MUTHIA <muthiamaylaa@gmail.com>
2025-05-27 15:14:00 +0700

    assalamualaikum kak rafi, ini namecard punya muthia. makasih sebelumnya kk

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import RafiNameCard from "../components/name-card/rafi";
+import MuteaNamecard from "../components/name-card/mutea";
 
 function AboutPage() {
   return (
@@ -11,7 +12,7 @@ function AboutPage() {
         <div className="flex gap-8 flex-wrap items-center justify-center w-full px-4">
         
         <RafiNameCard />
-        {/* Panggil kartu nama kamu dibawah sini..... */}
+        <MuteaNamecard />
 
 
         </div>
