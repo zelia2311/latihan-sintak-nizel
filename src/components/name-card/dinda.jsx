ca3c1637681f6fb33c9c6dc05968753779e9836d
ddindae <adindaoppo21@gmail.com>
2025-05-27 15:11:27 +0700

    mengubah foto

--- src/components/name-card/dinda.jsx
+++ src/components/name-card/dinda.jsx
@@ -7,21 +7,20 @@ function DindaNamecard() {
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-2">
               <img
-                src="./rafi.png"
+                src="./Adin.jpg"
                 alt="rafi"
                 className="w-18 rounded-full border border-gray-300 hover:animate-spin"
               />
               <div>
-                <h1 className="font-bold text-lg">Kgs. M. Rafiussyakban</h1>
+                <h1 className="font-bold text-lg">Adinda Rahmalia</h1>
                 <h2 className="text-sm">Manajemen Informatika</h2>
               </div>
             </div>
             <p className="text-gray-500">
-              "Halo, perkenalkan nama saya Kgs. M. Rafiussyakban saya adalah salah
-              satu mentor dari SINTAK 2025. Salam kenal semuanya...  "
+              "Halo, perkenalkan nama saya Adinda Rahmalia Salam kenal semuanya...  "
             </p>
             <a
-              href="https://www.instagram.com/rafius.rafi/" target="blank"
+              href="https://www.instagram.com/ddindae_/" target="blank"
               className="flex items-center justify-center gap-2 bg-black text-white py-2 rounded-md animate-bounce"
             >
                 <Instagram size={18} />
